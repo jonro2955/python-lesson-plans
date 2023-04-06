1599176dd9000ebb36e535cd807a54ea6fe6b8ec
jonro2955 <jonro.2955@gmail.com>
2023-04-06 11:10:52 -0700

    Fix color mode in spiral-star

--- turtle-drawings/spiral-star.py
+++ turtle-drawings/spiral-star.py
@@ -3,20 +3,18 @@ from turtle import Turtle, Screen
 turtle = Turtle()
 turtle.speed(0)
 screen = Screen()
+screen.colormode(255)
 
 R = 255
 G = 0
 B = 0
 
-screen.bgcolor((255, 255, 255))
-
 for i in range(255):
     G += 1
     B += 1
     R -= 1
-    turtle.color((R, G, B))
-    turtle.forward(i*2)
+    turtle.pencolor(R, G, B)
+    turtle.forward(i * 2)
     turtle.right(98)
-    print(i)
 
 screen.exitonclick()
